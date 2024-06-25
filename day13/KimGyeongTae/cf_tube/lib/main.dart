f0d6f1bb178faa183f960a010443f999d8948099
KimGyeongTae00 <gtkim@ecis.co.kr>
2024-06-25 16:22:43 +0900

    day13source

---
+++ day13/KimGyeongTae/cf_tube/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:cf_tube/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
