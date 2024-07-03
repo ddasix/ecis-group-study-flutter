a585d4831fad4523b8d55a9ce6bcdabe3e6a3819
susnake2 <susnake@ecis.co.kr>
2024-07-03 10:39:44 +0900

    day16

---
+++ day16/KimSuBeom/calendar_scheduler/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+import 'package:calendar_scheduler/repository/schedule_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final repository = ScheduleRepository();
+    final scheduleProvider = ScheduleProvider(repository: repository);
+
+    return ChangeNotifierProvider(
+      create: (_) => scheduleProvider,
+      child: MaterialApp(
+        title: 'calendar scheduler',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ day16/KimSuBeom/calendar_scheduler/lib/screen/home_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:get_it/get_it.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<ScheduleProvider>();
+    final selectedDate = provider.selectedDate;
+    final schedules = provider.cache[selectedDate] ?? [];
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
+            isScrollControlled: true,
+          );
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: SafeArea(
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              MainCalendar(
+                selectedDate: selectedDate,
+                onDaySelected: (selectedDate, focusedDate) =>
+                    onDaySelected(selectedDate, focusedDate, context),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TodayBanner(
+                selectedDate: selectedDate,
+                count: schedules.length,
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: schedules.length,
+                    itemBuilder: (context, index) {
+                      final schedule = schedules[index];
+                      return Dismissible(
+                        key: ObjectKey(schedule.id),
+                        direction: DismissDirection.startToEnd,
+                        onDismissed: (DismissDirection direction) {
+                          provider.deleteSchedules(
+                              date: selectedDate, id: schedule.id);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 8,
+                            left: 8,
+                            right: 8,
+                          ),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(
+      DateTime selectedDate, DateTime focusedDate, BuildContext context) {
+    final provider = context.read<ScheduleProvider>();
+    provider.changeSelectedDate(
+      date: selectedDate,
+    );
+    provider.getSchedules(date: selectedDate);
+  }
+}
