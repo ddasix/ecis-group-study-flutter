346151efe4df4ef22904e4437c0052c8a02442af
Shin Hyosik <hyosik@ecis.co.kr>
2024-07-05 13:22:46 +0900

    day17

--- day13/ShinHyosik/cf_tube/lib/constants/api.dart
+++ day13/ShinHyosik/cf_tube/lib/constants/api.dart
@@ -1,4 +1,4 @@
-const API_KEY = 'AIzaSyBkeIpW2Kg89FaqF72sTcMHmLNBL7VBoOU';
+const API_KEY = '';
 const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
 // const CF_CHANNEL_ID = 'UCs860BjHVQodQlp7TYiHyUw';
